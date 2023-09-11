cfec9d726fe65e8448c06a14e4fb6a2ee68c0127
Dejana Pivac <dejanapivac@gmail.com>
2023-09-11 16:15:00 +0200

    Added item inspection for Room scene

--- Assets/ExamineItem.cs
+++ Assets/ExamineItem.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExamineItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Examine Fields")]
+    //examine window obj
+    public GameObject examineWindow;
+    public GameObject mainWindow;
+    //refrence image and text
+    public Image examineImage;
+    public Text examineText;
+    public bool isExamining = false;
+
+    //ovo je valjda item skripta
+    public void Examine(Item item)
     {
-        
+        if (isExamining)
+        {
+            Debug.Log("Close");
+            examineWindow.SetActive(false);
+            mainWindow.SetActive(true);
+            isExamining = false;
+        }
+        else
+        {
+            SpriteRenderer spriteRenderer = item.transform.GetChild(0).GetComponent<SpriteRenderer>();
+
+            if (spriteRenderer != null)
+            {
+                Debug.Log("Examine");
+                examineImage.sprite = spriteRenderer.sprite;
+                examineText.text = item.descriptionText;
+                mainWindow.SetActive(false);
+                examineWindow.SetActive(true);
+                isExamining = true;
+            }
+            else
+            {
+                Debug.LogError("SpriteRenderer not found on child object of item.");
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
